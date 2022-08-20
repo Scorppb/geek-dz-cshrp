8dd77b1950e4f9ddef6e6b9cb5a8a8404460c5f5
Scorppb <scorppb@yandex.ru>
2022-08-20 10:13:45 +0300

    tsk01

---
+++ 04/tsk01/Program.cs
@@ -0,0 +1,36 @@
+﻿// Функция, на вход получает параметр, на выходе введженнео с консоли значение
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  // Считавает с консоли строку
+    int result = int.Parse(readValue);      // Преобрадует строку в целое число
+    return result;                          // Возвращает результат
+}
+
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
+// в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponentiation(int number, int degree)
+{
+    int result = 1;
+    for (int i = 0; i < degree; i++)
+    {
+        result = result * number;
+    }
+    return result;
+}
+
+
+
+int number = Prompt("Введите число A, которое неоходимо возводить в степень> ");
+int degree = Prompt("Введите число B, которое соответствует натуральной степени> ");
+if (degree > 0)
+{
+    System.Console.WriteLine($"Если число {number} возвести в натуральную степень {degree} получим результат {Exponentiation(number, degree)}");
+}
+else
+{
+    System.Console.WriteLine("Число B не соответствует натуральному");
+}
