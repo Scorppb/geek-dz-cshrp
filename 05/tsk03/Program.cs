560c17e237a98348665fd4f44cb6a40686525e5b
Scorppb <scorppb@yandex.ru>
2022-08-20 15:38:37 +0300

    check error

--- 05/tsk03/Program.cs
+++ 05/tsk03/Program.cs
@@ -16,7 +16,7 @@ double[] genArray(int length)
     double[] array = new double[length];
     for (int i = 0; i < length; i++)
     {
-        array[i] = new Random().NextDouble()*100;
+        array[i] = new Random().NextDouble() * 100;
     }
     return array;
 }
@@ -25,10 +25,35 @@ void printArray(double[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        System.Console.Write(Math.Round(array[i],1) + " ");
+        System.Console.Write(Math.Round(array[i], 1) + " ");
     }
 }
+
+double findMax(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i]) max = array[i];
+    }
+    return max;
+}
+
+double findMin(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
+}
+
+
 int lenghtArray = Prompt("Введите длинну массива> ");
 double[] array = genArray(lenghtArray);
 printArray(array);
+findMax(array);
+findMin(array);
+System.Console.WriteLine($"\nРазницу между максимальным и минимальным элементов массива {Math.Round((findMax(array))-(findMin(array)),1)}");
 
