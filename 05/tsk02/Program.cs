a2e9a75a7aacadfdf0290c3eb3212ba958d48fa5
Scorppb <scorppb@yandex.ru>
2022-08-20 14:51:51 +0300

    tsk03

--- 05/tsk02/Program.cs
+++ 05/tsk02/Program.cs
@@ -32,7 +32,7 @@ void printArray(int[] array)
 int sumUnfairPositionArray(int[] array)
 {
     int sum = 0;
-    for (int i = 0; i < array.Length; i += 2)
+    for (int i = 1; i < array.Length; i +=2)
     {
         sum += array[i];
     }
---
+++ 05/tsk03/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным 
+// элементов массива.
+// [3 7.3 22.2 2 78] -> 76
+
+
+int Prompt(string massage)
+{
+    System.Console.Write(massage);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+double[] genArray(int length)
+{
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().NextDouble()*100;
+    }
+    return array;
+}
+
+void printArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(Math.Round(array[i],1) + " ");
+    }
+}
+int lenghtArray = Prompt("Введите длинну массива> ");
+double[] array = genArray(lenghtArray);
+printArray(array);
+
