a156fe4cf9e9d181739c567a618195689f6bb136
Scorppb <scorppb@yandex.ru>
2022-08-20 10:32:13 +0300

    tsk02

--- 04/tsk02/Program.cs
+++ 04/tsk02/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Функция, на вход получает параметр, на выходе введженнео с консоли значение
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  // Считавает с консоли строку
+    int result = int.Parse(readValue);      // Преобрадует строку в целое число
+    return result;                          // Возвращает результат
+}
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumDigitOfNumber(int number)
+{
+    int result = 0;
+    for (int i = 0; number > 0; i++)
+    {
+        int temp = number % 10;
+        number = number / 10;
+        result = result + temp;
+    }
+    return result;
+}
+
+int number = Prompt("Введите число> ");
+if (number >= 0)
+{
+    System.Console.WriteLine($"Сумма цифр в числе {number} равна {SumDigitOfNumber(number)}");
+}
+else
+{
+    int numberMinus=number*(-1);
+    System.Console.WriteLine($"Сумма цифр в числе {number} равна {SumDigitOfNumber(numberMinus)}");
+}
+
