0f4621b798703531a68c773e9ade18f50884a7d2
Scorppb <scorppb@yandex.ru>
2022-08-29 15:35:33 +0300

    dz 06 tsk03

--- 06/tsk03/Program.cs
+++ 06/tsk03/Program.cs
@@ -1,2 +1,40 @@
 ﻿// Сгенерировать двумерный массив со случайными числами, вывести его на экран, оформить в виде 
 // функций (пригодится в дальнейшем)
+
+
+int Prompt(string massage)
+{
+    System.Console.Write(massage);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int[,] GenMatrix(int line, int columns)
+{
+    int[,] matrix = new int[line, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-100, 100);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int line = Prompt("Введите количество строк> ");
+int columns = Prompt("Введите количество столбцов> ");
+PrintMatrix(GenMatrix(line, columns));
