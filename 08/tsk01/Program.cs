1fac00cd407f35c40e29f4fd2a2ed5674bd1ed8e
Scorppb <scorppb@yandex.ru>
2022-09-03 16:57:43 +0300

    start

---
+++ 08/tsk01/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+int Prompt(string massage)
+{
+    System.Console.Write(massage);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int[,] GenMatrix(int line, int columns, int min, int max)
+{
+    int[,] matrix = new int[line, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+
+
+int line = Prompt("Введите количество строк> ");
+int columns = Prompt("Введите количество столбцов> ");
+if (line <= 0 || columns <= 0) System.Console.WriteLine("Введите число больше 0");
+else
+{
+    int min = Prompt("Ведите минимальное допустимое значение> ");
+    int max = Prompt("Ведите максимальное допустимое значение> ");
+    int[,] matrix = GenMatrix(line, columns, min, max);
+    PrintMatrix(matrix);
+    System.Console.WriteLine();
+
+}
